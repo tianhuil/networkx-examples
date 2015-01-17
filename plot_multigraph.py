a1a2f936eedcd3f8fad46e7225372100bd7967bb
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2015-01-17 16:23:20 -0500

    Finishing classic graphs

--- plot_multigraph.py
+++ plot_multigraph.py
@@ -1,12 +1,13 @@
 import networkx as nx
 import matplotlib.pylab as plt
 
-def plot_multigraph(graphs, n_rows, n_cols, node_size=100, fig_no=1):
+def plot_multigraph(graphs, n_rows, n_cols, node_colors=None, node_size=100, fig_no=1):
   fig = plt.figure(fig_no)
   fig.clear()
   for k, (name, G) in enumerate(graphs):
+    node_colors = node_color[k] if node_colors else 'r'
     plt.subplot(n_rows, n_cols, k + 1)
     plt.title(name)
-    nx.draw(G, node_size=node_size)
+    nx.draw(G, node_color=node_color, node_size=node_size)
 
   plt.show()
