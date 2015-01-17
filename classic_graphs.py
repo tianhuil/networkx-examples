db16541df56cc376e4458f5920c9f6866c9f98a9
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2015-01-17 16:16:35 -0500

    Finishing classic graphs

--- classic_graphs.py
+++ classic_graphs.py
@@ -8,6 +8,8 @@ r = 2
 h = 3
 dim = [2, 3]
 
+# left out dorogovtsev_goltsev_mendes_graph and null_graph
+
 graphs = [
   ("balanced_tree", nx.balanced_tree(r, h)),
   ("barbell_graph", nx.barbell_graph(n, m)),
@@ -15,14 +17,12 @@ graphs = [
   ("complete_bipartite_graph", nx.complete_bipartite_graph(n, m)),
   ("circular_ladder_graph", nx.circular_ladder_graph(n)),
   ("cycle_graph", nx.cycle_graph(n)),
-  # ("dorogovtsev_goltsev_mendes_graph", nx.dorogovtsev_goltsev_mendes_graph(n)),
   ("empty_graph", nx.empty_graph(n)),
   ("grid_2d_graph", nx.grid_2d_graph(m, n)),
   ("grid_graph", nx.grid_graph(dim)),
   ("hypercube_graph", nx.hypercube_graph(hypercube_n)),
   ("ladder_graph", nx.ladder_graph(n)),
   ("lollipop_graph", nx.lollipop_graph(m, n)),
-  # ("null_graph", nx.null_graph()),
   ("path_graph", nx.path_graph(n)),
   ("star_graph", nx.star_graph(n)),
   ("trivial_graph", nx.trivial_graph()),
