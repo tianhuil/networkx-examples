8ef6909d00734ba56deee9dfcda025302564d5b1
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2015-01-17 17:13:13 -0500

    More centrality

--- centrality.py
+++ centrality.py
@@ -1,5 +1,4 @@
 import networkx as nx
-from networkx import algorithms
 import plot_multigraph
 from matplotlib import pylab as plt
 
@@ -11,11 +10,16 @@ def to_list(dict_):
   return [dict_[k] for k in G.nodes()]
 
 graph_colors = [
-  ("degree", to_list(algorithms.degree_centrality(G))),
-  ("betweenness", to_list(algorithms.betweenness_centrality(G))),
-  ("load", to_list(algorithms.load_centrality(G))),
-  ("eigenvector", to_list(algorithms.eigenvector_centrality(G))),
+  ("degree", to_list(nx.degree_centrality(G))),
+  ("betweenness", to_list(nx.betweenness_centrality(G))),
+  ("load", to_list(nx.load_centrality(G))),
+  ("eigenvector", to_list(nx.eigenvector_centrality_numpy(G))),
+  ("closeness_centrality", to_list(nx.closeness_centrality(G))),
+  ("current_flow_closeness", to_list(nx.current_flow_closeness_centrality(G))),
+  ("current_flow_betweenness", to_list(nx.current_flow_betweenness_centrality(G))),
+  ("katz", to_list(nx.katz_centrality_numpy(G))),
+  ("communicability", to_list(nx.communicability_centrality(G))),
 ]
 
-plot_multigraph.plot_color_multigraph(G, graph_colors, 2, 2, node_size=50)
+plot_multigraph.plot_color_multigraph(G, graph_colors, 3, 3, node_size=50)
 plt.show()
--- plot_multigraph.py
+++ plot_multigraph.py
@@ -17,7 +17,7 @@ def plot_color_multigraph(G, graph_colors, n_rows, n_cols, node_size=100, fig_no
   fig.clear()
   for k, (name, colors) in enumerate(graph_colors):
     plt.subplot(n_rows, n_cols, k + 1)
-    plt.title(name)
+    plt.title(name, color='white')
     nx.draw_graphviz(G,  # this is expensive but at least it's consistent.
       node_color=colors,
       vmin=min(colors),
