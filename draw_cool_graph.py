73a7d54a556b05b24a1da7efb7827ee5467d566f
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2015-01-17 11:18:21 -0500

    Fixing Bug

--- draw_cool_graph.py
+++ draw_cool_graph.py
@@ -1,7 +1,7 @@
 import networkx as nx
 import matplotlib.pylab as plt
 
-def draw_cool_graph(G, fig_no=1):
+def draw_cool_graph(G, fig_no=1, node_size=100):
   colors = [deg for _, deg in G.degree_iter()]
   fig = plt.figure(fig_no)
   fig.clear()
@@ -10,14 +10,16 @@ def draw_cool_graph(G, fig_no=1):
     vmin=min(colors),
     vmax=max(colors),
     cmap=plt.get_cmap('cool'),
-    node_size=100,
+    node_size=node_size,
     alpha=.5,
-    edge_color="#FFFFFF")
+    edge_color="#FFFFFF"
+  )
   fig.set_facecolor("#000000")
-  plt.show()
+  return fig
 
-if __name__ == '__name__':
+if __name__ == '__main__':
   n = 100
   p = 10. / n
   G = nx.fast_gnp_random_graph(n, p)
-  draw_cool_graph(G)
+  fig = draw_cool_graph(G, node_size=20)
+  plt.savefig('graphs/cool_graph.png', facecolor=fig.get_facecolor())
